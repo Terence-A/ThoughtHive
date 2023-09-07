21a10dac52f5b3e74d7032028f7a488598ab6a9d
terenceDev <terence.arseneault@hotmail.com>
2023-09-07 15:57:27 -0300

    set up api routes and made test endpoints and reaching the endpoints in user and thought api routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.send("Home route working!");
+  res.send("User Get request reached");
 });
 
 module.exports = router;

7d44a1e6c72535065a590c0911563a1f13fee8f8
terenceDev <terence.arseneault@hotmail.com>
2023-09-07 15:36:47 -0300

    Created models

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: Types.ObjectId,
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => new Date(timestamp).toDateString(),
+  },
+});
+
+const Reaction = model("reaction", reactionSchema);
+
+module.exports = Reaction;
